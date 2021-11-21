f942e12b4f4fbec559662e2822acbb4236cc13a8
Sofia W <sofiawberg@live.se>
2021-11-21 21:18:31 +0100

    did the codealong

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components/macro';
+
+import { Card } from 'lib/Card';
+
+const ThinnerCard = styled(Card)`
+  width: 400px;
+`;
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <ThinnerCard
+        coverImage='https://www.fillmurray.com/500/300'
+        thumbnailUrl='https://www.fillmurray.com/100/100'
+        title='this is the title'
+        secondaryText='this is the secondary text'
+      >
+        also children
+      </ThinnerCard>
+
+      <Card title='only a title' secondaryText='secondary text' />
+
+      <Card>
+        <h1>Hello from children</h1>
+      </Card>
     </div>
-  )
-}
+  );
+};
